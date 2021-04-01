38466875e8b756cf59228f855257d0e9160f9e38
Poonam Dahiya <poonam.dahiya@applitools.com>
2021-04-01 11:39:14 +1100

    New browser config

--- applitools.config.js
+++ applitools.config.js
@@ -9,8 +9,8 @@ module.exports = {
     // { width: 800, height: 600, name: "firefox" },
     // {width: 800, height: 600, name: 'edge'},
     // {width: 800, height: 600, name: 'ie11'},
-    { width: 1200, height: 800, name: "chrome" }
-    // { width: 1200, height: 800, name: "firefox" }
+     { width: 1200, height: 800, name: "chrome" }
+     { width: 1200, height: 800, name: "safari" }
     // {width: 1200, height: 800, name: 'edge'},
     // {width: 1200, height: 800, name: 'ie11'},
     // { deviceName: "iPhone X" }
