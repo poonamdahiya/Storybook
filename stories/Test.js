3cc702ff832fc5f5db1381e316994d3a5f1333e3
Poonam Dahiya <poonam.dahiya@applitools.com>
2022-01-20 19:04:11 +1100

    tset

--- stories/Test.js
+++ stories/Test.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Test({ index }) {
   return (
     <React.Fragment>
-      <div className="blue">We've met already, I'm Green {index}</div>
+      <div className="blue">We've met already, I'm Green New One {index}</div>
       <div className="font-zilla" style={{ fontSize: 36 }}>
         This is Zilla Slab font
       </div>
